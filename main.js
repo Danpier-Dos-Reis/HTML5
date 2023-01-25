2c03f248da6015bd7b4b8d74e68cecd15c550e89
Danpier Dos Reis <d6nt3_d3m0n@outlook.com>
2023-01-25 11:49:01 -0300

    Manipuladon evento 'mouseover' con TS

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ let buttons = document.getElementsByTagName("button");
 function changeColor(element) {
     element === null || element === void 0 ? void 0 : element.setAttribute("style", "background-color: red;");
 }
+function printAlert() {
+    alert("I also Love JavaScript"); //Third Button execute this
+}
+function cardsOver(element) {
+    element.style.backgroundColor = "#84F46FB0";
+}
+//#region  Buttons *click* event
 buttons[0].addEventListener("click", (e) => {
     // " e.CurrentTarget " se refiere al elemento que disparó el evento
     let parent = e.currentTarget;
@@ -15,15 +22,20 @@ buttons[1].addEventListener("click", () => {
     let parent = buttons[1].parentElement;
     changeColor(parent);
 });
-buttons[2].addEventListener("click", (button) => {
-    let boton = button.currentTarget;
-    let parent = boton.parentElement; // parent = <div class="container">
-    changeColor(parent);
-});
+buttons[2].addEventListener("click", printAlert);
 buttons[3].addEventListener("click", (element) => {
     let parent = element.currentTarget;
     parent = parent.parentElement;
     changeColor(parent);
 });
-let padre = buttons[0].parentElement;
-console.log(padre);
+//#endregion
+//#region  Cards  *mouseover* event
+let firstCard = document.querySelector(".first");
+let secondCard = document.querySelector(".second");
+let thirdCard = document.querySelector(".third");
+let forthCard = document.querySelector(".forth");
+firstCard.addEventListener("mouseover", (e) => { cardsOver(e.currentTarget); });
+secondCard.addEventListener("mouseover", (e) => { cardsOver(e.currentTarget); });
+thirdCard.addEventListener("mouseover", (e) => { cardsOver(e.currentTarget); });
+forthCard.addEventListener("mouseover", (e) => { cardsOver(e.currentTarget); });
+//#endregion
--- main.ts
+++ main.ts
@@ -3,31 +3,45 @@ let buttons = document.getElementsByTagName("button");
 function changeColor(element :HTMLElement){
     element?.setAttribute("style","background-color: red;");
 }
-    
-    buttons[0].addEventListener("click", (e) => {
-        // " e.CurrentTarget " se refiere al elemento que disparó el evento
-        let parent:HTMLElement = e.currentTarget as HTMLElement;
-            parent = parent.parentElement as HTMLElement;
-        changeColor(parent);
-    });
+function printAlert(){
+    alert("I also Love JavaScript"); //Third Button execute this
+}
+function cardsOver(element:HTMLElement){
+    element.style.backgroundColor = "#84F46FB0";
+}
 
-    /*" e " es opcional, solo es necesario en ciertos casos como el anterior
-        donde fué necesario hacer referencia al elemento disparador*/
-    buttons[1].addEventListener("click", () => {
-        let parent : HTMLElement = buttons[1].parentElement  as HTMLElement;
-        changeColor(parent);
-    });
+//#region  Buttons *click* event
+buttons[0].addEventListener("click", (e) => {
+    // " e.CurrentTarget " se refiere al elemento que disparó el evento
+    let parent:HTMLElement = e.currentTarget as HTMLElement;
+        parent = parent.parentElement as HTMLElement;
+    changeColor(parent);
+});
 
-    buttons[2].addEventListener("click", (button) => {
-        let boton:HTMLElement = button.currentTarget as HTMLElement;
-        let parent = boton.parentElement as HTMLElement; // parent = <div class="container">
-        changeColor(parent);
-    });
-    buttons[3].addEventListener("click", (element) => {
-        let parent:HTMLElement = element.currentTarget as HTMLElement;
-            parent = parent.parentElement as HTMLElement;
-        changeColor(parent);
-    });
+/*" e " es opcional, solo es necesario en ciertos casos como el anterior
+    donde fué necesario hacer referencia al elemento disparador*/
+buttons[1].addEventListener("click", () => {
+    let parent : HTMLElement = buttons[1].parentElement  as HTMLElement;
+    changeColor(parent);
+});
 
-let padre = buttons[0].parentElement;
-console.log(padre as HTMLElement);
+buttons[2].addEventListener("click", printAlert);
+buttons[3].addEventListener("click", (element) => {
+    let parent:HTMLElement = element.currentTarget as HTMLElement;
+        parent = parent.parentElement as HTMLElement;
+    changeColor(parent);
+});
+//#endregion
+
+//#region  Cards  *mouseover* event
+let firstCard:HTMLElement = document.querySelector(".first") as HTMLElement;
+let secondCard:HTMLElement = document.querySelector(".second") as HTMLElement;
+let thirdCard:HTMLElement = document.querySelector(".third") as HTMLElement;
+let forthCard:HTMLElement = document.querySelector(".forth") as HTMLElement;
+
+firstCard.addEventListener("mouseover", (e) => {cardsOver(e.currentTarget as HTMLElement)});
+secondCard.addEventListener("mouseover", (e) => {cardsOver(e.currentTarget as HTMLElement)});
+thirdCard.addEventListener("mouseover", (e) => {cardsOver(e.currentTarget as HTMLElement)});
+forthCard.addEventListener("mouseover", (e) => {cardsOver(e.currentTarget as HTMLElement)});
+
+//#endregion
