28d7c1f76f02958656d6218a08a6b67da721b36d
Danpier Dos Reis <d6nt3_d3m0n@outlook.com>
2023-01-26 17:30:28 -0300

    Delegacion de eventos

--- local_assets/main.ts
+++ local_assets/main.ts
@@ -1,37 +1,13 @@
-//#region Handling  Propagation
-const buttons = document.querySelector(".button") as HTMLElement;
-//#region Nota
-    // Al windows tener la propiedad *once* en TRUE su evento CLICK se ejecuta de
-    //último
-//#endregion
+//#region Event Delegation
+const btn = document.querySelector("#container") as HTMLElement;
 
-window.addEventListener("click",()=>{
-    console.log("Document/Window click event");
-}, false ); //once es una palabra reservada de JavaScript y se refiere a una propiedad
+btn?.addEventListener("click", function(event ) {
+   const element:HTMLElement = event.target as HTMLElement;
+   console.log(element.getAttribute("id")+" is CLICKED");
 
-document.addEventListener("click",() => {
-    console.log("Document click event");
-}, false );
+   if(element.matches("li")){
+        element.style.backgroundColor = "red";
+   }
+});
 
-document.querySelector(".firstcontainer")?.addEventListener("click",() => {
-    console.log("Yellow Container")
-}, false );
-
-document.querySelector(".container2")?.addEventListener("click",(element) => {
-    console.log("Green Container");
-}, { once : true } );
-
-document.querySelector(".firstCard")?.addEventListener("click",() => {
-    console.log("Card Container")
-}, false );
-
-buttons?.addEventListener("click",(event , btn? : HTMLElement) => {
-    /* Nota
-    Quita la acción por defecto del navegador que es recargar la página porque el href del
-    link está vació
-    */
-   event.preventDefault(); //preventDefault() es una función del evento, no del elemento html
-    btn = event.currentTarget as HTMLElement; //tenemos el elemento partiendo de su evento
-    console.log(btn.innerHTML = "¡¡¡¡Clicked little button!!!!"); //innerText es una función de un elemento html no de un evento
-}, false );
 //#endregion
--- main.js
+++ main.js
@@ -1,33 +1,11 @@
 "use strict";
-var _a, _b, _c;
-//#region Handling  Propagation
-const buttons = document.querySelector(".button");
-//#region Nota
-// Al windows tener la propiedad *once* en TRUE su evento CLICK se ejecuta de
-//último
-//#endregion
-window.addEventListener("click", () => {
-    console.log("Document/Window click event");
-}, false); //once es una palabra reservada de JavaScript y se refiere a una propiedad
-document.addEventListener("click", () => {
-    console.log("Document click event");
-}, false);
-(_a = document.querySelector(".firstcontainer")) === null || _a === void 0 ? void 0 : _a.addEventListener("click", () => {
-    console.log("Yellow Container");
-}, false);
-(_b = document.querySelector(".container2")) === null || _b === void 0 ? void 0 : _b.addEventListener("click", (element) => {
-    console.log("Green Container");
-}, { once: true });
-(_c = document.querySelector(".firstCard")) === null || _c === void 0 ? void 0 : _c.addEventListener("click", () => {
-    console.log("Card Container");
-}, false);
-buttons === null || buttons === void 0 ? void 0 : buttons.addEventListener("click", (event, btn) => {
-    /* Nota
-    Quita la acción por defecto del navegador que es recargar la página porque el href del
-    link está vació
-    */
-    event.preventDefault();
-    btn = event.currentTarget;
-    console.log(btn.innerHTML = "¡¡¡¡Clicked little button!!!!");
-}, false);
+//#region Event Delegation
+const btn = document.querySelector("#container");
+btn === null || btn === void 0 ? void 0 : btn.addEventListener("click", function (event) {
+    const element = event.target;
+    console.log(element.getAttribute("id") + " is CLICKED");
+    if (element.matches("li")) {
+        element.style.backgroundColor = "red";
+    }
+});
 //#endregion
