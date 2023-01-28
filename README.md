9657259bed1df99da8f7d5c96066f7ff03ca2527
Danpier Dos Reis <d6nt3_d3m0n@outlook.com>
2023-01-28 19:25:53 -0300

    Proyecto 2 terminado 100% real no fake

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # HTML5 Knowledges
 Initial Commit **_Free Code Camp Projects_**
+
+## Funciones
+
+* _Math_.**floor()** : redondea el resultado de una operación aritmética al valor más bajo.
+    * Ejemplo: Math.floor(1.9) = 1
+* _Math_.**random()** = genera un número aleatorio entre 0 (inclusive) y 1 (exclusivo). Al multiplicarlo por el tamaño del array quotes.length se está asegurando que el número generado aleatoriamente sea menor al tamaño del array, y asi poder utilizarlo como un indice del array.
+    * Más : si queremos generar un número aleatorio entre el 1 y el 5, de manera inclusiva, podríamos utilizar el método **_Math.floor(Math.random() * 5) + 1_**, lo cual nos daría un número aleatorio entre y 5, ambos incluidos. Si queremos generar un número aleatorio entre el 1 y el 5 de manera exclusiva, podríamos utilizar el método **_Math.floor(Math.random() * 4) + 1_**, lo cual nos daría un número aleatorio entre 1 y 4, el 5 no estaría incluido en el rango.
+## Css
+* _position_ : **fixed ;** = La propiedad _"position: fixed"_ en CSS establece que un elemento estará posicionado en relación a la ventana del navegador y no a su contenedor padre. Esto significa que aunque el usuario haga scroll en la página, el elemento con "position: fixed" seguirá en la misma posición en la pantalla.
--- src/main.ts
+++ src/main.ts
@@ -1,37 +1,12 @@
-const quotes = [
-    {
-        quote : "I don't think that you have any insight whatsoever into your capacity for good until you have some well-developed insight into your capacity for evil.",
-        author : "Jordan B. Peterson"
-    },
-    {
-        quote : "A problem defined, is a problem half solved.",
-        author : "Albert Einstein"
-    },
-    {
-        quote : "Darkness cannot drive out darkness: only light can do that. Hate cannot drive out hate: only love can do that.",
-        author : "Martin Luther King"
-    },
-    {
-        quote : "Be the change that you wish to see in the world.",
-        author : "Mahatma Gandhi"
-    },
-    {
-        quote : "Y yo lo entiendo porque también lo he vivido, el mundo está lleno de gente que camina sin sentido ¿Se te hace duro pensar que exista otro ser vivo que valga la pena entregarle tus latidos?",
-        author : "Canserbero"
-    }];
+const btn = document.querySelector("#principalBTN");
+const alerta = document.querySelector("#alert") as HTMLElement;
+const cerrar = document.querySelector("span")
 
-let btnLoad = document.getElementById("btnload");
-const textQuote = document.querySelector(".quote") as HTMLElement;
-let textAuthor = document.querySelector(".author") as HTMLElement;
-
-btnLoad?.addEventListener("click" , () => {
-    let rdn = Math.floor(Math.random() * quotes.length);
-    let txtQuote:string = quotes[rdn].quote
-    let txtAuthor:string = quotes[rdn].author
-
-    textQuote.textContent = txtQuote
-    textAuthor.textContent = txtAuthor
+btn?.addEventListener("click" , () => {
+    alerta.setAttribute("style","bottom: 50%; transition: 0.5s;");
 });
 
-
-
+cerrar?.addEventListener("click" , () => {
+    alerta.removeAttribute("style")
+    alerta.setAttribute("style" , "bottom: 100%; transition: 0.5s;")
+});
